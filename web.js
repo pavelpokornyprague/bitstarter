1c6f153602b66cb882d7df83f2e3741e0b9dc119
Pavel Pokorny <pavel.pokorny.prague@gmail.com>
2013-07-13 23:41:03 +0000

    Changed web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,16 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+
+  buf = new Buffer(65000);
+
+  buf = fs.readFileSync("index.html");
+  console.log("read from file::" + buf.toString());
+
+
+  response.send(buf.toString());
+
 });
 
 var port = process.env.PORT || 5000;
